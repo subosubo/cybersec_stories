08167d85f9d2ac1015eb3ff4cbca59008459e12e
subo subo <48369401+subosubo@users.noreply.github.com>
2022-10-31 12:52:07 +0000

    minor update

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def sendtowebhook(webhookurl: str, content: Embed):
         try:
             webhook = Webhook.from_url(webhookurl, session=session)
             await webhook.send(embed=content)
-        except RateLimited(float(600)):
+        except RateLimited(600):
             await webhook.send(embed=content)
 
 
