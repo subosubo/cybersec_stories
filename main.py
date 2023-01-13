42f1c55c94a9876ccda6cb94f2d83cf224063e9a
subosubo <subo0sub0sub0@gmail.com>
2023-01-14 01:13:12 +0800

    works!

--- main.py
+++ main.py
@@ -450,7 +450,7 @@ if __name__ == "__main__":
     scheduler = AsyncIOScheduler(timezone="Asia/Singapore")
     scheduler.add_job(
         # , hour="8-18", minute="*/3"
-        itscheckintime, "cron", day_of_week="mon-sun", hour="0-23", minute="*/3"
+        itscheckintime, "cron", day_of_week="mon-sun", hour="8-18", minute="*/30"
     )
     scheduler.start()
 
